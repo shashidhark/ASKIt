41212057c3a5a130e368c077a9d75e87d5ab3c12
Shashidhar <shashidharkulal@gmail.com>
2015-03-03 21:10:35 +0530

    some bugs Fixed

--- data/display/panel.js
+++ data/display/panel.js
@@ -1,7 +1,11 @@
 
 self.on('message', function onMessage(text) {
+	var result="";
 	if(text==0)
-		document.getElementById('mean').innerHTML="Sorry.. Meaning not found.";
-	else
-  		document.getElementById('mean').innerHTML=$(text).find('ul').html();
+		result = "Sorry.. Meaning not found.";
+	else{
+  		result = $(text).find('ul').eq(0).text();
+  	}
+  	document.getElementById('mean').innerHTML = result;
+  	self.port.emit('show', result);
 });
